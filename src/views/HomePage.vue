1fc0797779918d5ef144cefe68ceefc9a3fbe731
balejos2122 <biancaalejos17@gmail.com>
2024-02-19 03:51:07 -0600

    paginatio done, cannot verify due to firebase not wanting to install

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,8 +2,10 @@
 //import { ref } from 'vue'
 //const title = ref('Hello There ✋')
 import MainCardsVue from '@/components/MainCards.vue';
+import MainPaginationVue from '@/components/MainPagination.vue';
 </script>
 
 <template>
   <MainCardsVue></MainCardsVue>
+  <MainPaginationVue></MainPaginationVue>
 </template>
